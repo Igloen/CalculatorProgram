f4f62190fb7dfece560de6c5e11ba5eccec4bdde
Christoffer Vase Overgaard <au581166@uni.au.dk>
2020-02-05 15:57:33 +0100

    Added new methods.

--- CalculatorProgram/Calculator.cs
+++ CalculatorProgram/Calculator.cs
@@ -16,11 +16,24 @@ namespace CalculatorProgram
             return sum;
         }
 
+        public double AddTooAcumulator(double addend)
+        {
+            Accumulator = Accumulator + addend;
+            return Accumulator;
+        }
+
+
         public double Subtract(double a, double b)
         {
             double sum = a - b;
             Accumulator += sum;
             return sum;
+        }
+
+        public double SubtractFromAcumulator(double subtractor)
+        {
+            Accumulator = Accumulator - subtractor;
+            return Accumulator;
         }
 
         public double Multiply(double a, double b)
@@ -30,12 +43,25 @@ namespace CalculatorProgram
             return sum;
         }
 
+        public double MultiplyAcumulator(double multiplyer)
+        {
+            Accumulator = Accumulator * multiplyer;
+            return Accumulator;
+        }
+
         public double Power(double x, double exp)
         {
             double sum = Math.Pow(x, exp);
             Accumulator += sum;
             return sum;
         }
+
+        public double PowerAcumulator(double exponent)
+        {
+            Accumulator = Math.Pow(Accumulator,exponent);
+            return Accumulator;
+        }
+
 
         public double Divide(double dividend, double divisor)
         {
@@ -46,6 +72,12 @@ namespace CalculatorProgram
             double sum = dividend / divisor;
             Accumulator += sum;
             return sum;
+        }
+
+        public double DevideAcumulator(double divisor)
+        {
+            Accumulator = Accumulator/divisor;
+            return Accumulator;
         }
 
         public void Clear()
