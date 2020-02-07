e63707ffb8e659fe524d3adac54716778c1ef5c9
Mads <au579023@uni.au.dk>
2020-02-07 08:58:33 +0100

    Nye metoder opdateret, samt lavet test for dem alle.

--- Calculate.Test/CalculatorTest.cs
+++ Calculate.Test/CalculatorTest.cs
@@ -82,14 +82,16 @@ namespace Calculate.Test
 
         //********************** Accumulator *****************************
 
-        [Test]
-        public void Test_Accumulator()
+        [TestCase(2,5,17)]
+        [TestCase(8, 11, 107)]
+        [TestCase(1, 2, 5)]
+        public void Test_Accumulator(double v1, double v2, double result)
         {
-            // Act
-            double result = uut.Add(2, 5) + uut.Multiply(2, 5);
+            uut.Add(v1, v2);
+            uut.Multiply(v1, v2);
 
             // Assert
-            Assert.That(result, Is.EqualTo(17));
+            Assert.That(uut.Accumulator, Is.EqualTo(result));
         }
 
         //********************** Clear-Method *****************************
@@ -98,11 +100,91 @@ namespace Calculate.Test
         public void Test_Clear_Method()
         {
             // Act
+            uut.Add(2, 5);
             uut.Clear();
 
             // Assert
             Assert.That(uut.Accumulator, Is.EqualTo(0));
         }
+
+
+        //********************** AddTooAccumulator-Method *****************************
+
+
+        [TestCase(2, 5, 12)]
+        [TestCase(22, 2, 26)]
+        [TestCase(4, 23, 50)]
+        public void Add_Numbers_to_accumulator(double v1, double v2, double result)
+        {
+            // Act + Assert
+            uut.Add(v1, v2);
+
+          double testValue = uut.AddTooAcumulator(v2);
+
+            Assert.That(testValue,Is.EqualTo(result));
+        }
+
+
+        //********************** SubtractFromAccumulator-Method *****************************
+
+        [TestCase(3, 5, 1,7)]
+        [TestCase(22, 4, 7,19)]
+        [TestCase(4, 25, 10,19)]
+        public void Subtract_Numbers_from_accumulator(double v1, double v2, double v3, double result)
+        {
+            // Act + Assert
+            uut.Add(v1, v2);
+
+            double testValue = uut.SubtractFromAcumulator(v2);
+
+            Assert.That(testValue, Is.EqualTo(result));
+        }
+
+        //********************** MultiplyAccumulator-Method *****************************
+
+        [TestCase(3, 5, 5, 40)]
+        [TestCase(22, 4, 2, 52)]
+        [TestCase(4, 25, 10, 290)]
+        public void Multiply_accumulator(double v1, double v2, double v3, double result)
+        {
+            // Act + Assert
+            uut.Add(v1, v2);
+
+            double testValue = uut.MultiplyAcumulator(v3);
+
+            Assert.That(testValue, Is.EqualTo(result));
+        }
+
+        //********************** PowerOffAccumulator-Method *****************************
+
+        [TestCase(3, 5, 5, 32768)]
+        [TestCase(22, 4, 2, 676)]
+        [TestCase(4, 25, 3, 24389)]
+        public void Power_Off_accumulator(double v1, double v2, double v3, double result)
+        {
+            // Act + Assert
+            uut.Add(v1, v2);
+
+            double testValue = uut.PowerAcumulator(v3);
+
+            Assert.That(testValue, Is.EqualTo(result));
+        }
+
+        //********************** DevideAccumulator-Method *****************************
+
+        [TestCase(3, 5, 5, 1.6)]
+        [TestCase(22, 4, 2, 13)]
+        [TestCase(3, 12, 3, 5 )]
+        public void Devide_accumulator(double v1, double v2, double v3, double result)
+        {
+            // Act + Assert
+            uut.Add(v1, v2);
+
+            double testValue = uut.DevideAcumulator(v3);
+
+            Assert.That(testValue, Is.EqualTo(result));
+        }
+
 
     }
 
